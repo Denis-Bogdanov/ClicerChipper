31c82660da4471d67dc9ff078c55c7caa5140cdf
Dennis Bogdanov <d.bogdanov@corp.mail.ru>
2019-06-18 15:26:51 +0300

    Start

---
+++ Assets/GameScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Enemy;
+    public Vector3 Position;
+
+    private GameObject _spawnedEnemy;
+
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    public void SpawnEnemy()
+    {
+        _spawnedEnemy = Instantiate(Enemy[Random.Range(0, Enemy.Length)], Position, Quaternion.identity);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            DestroyEnemy();
+        }
+    }
+
+    [UsedImplicitly]
+    public void DestroyEnemy()
+    {
+        Destroy(_spawnedEnemy);
+        SpawnEnemy();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
