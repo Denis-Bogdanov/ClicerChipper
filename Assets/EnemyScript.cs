c6bdf5aa1ac3ffea7bf91a2fabe6b36094fa0711
Dennis Bogdanov <d.bogdanov@corp.mail.ru>
2019-06-24 10:28:49 +0300

    Add script

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private Action _callback;
+    private GameScript _gameScript;
+
+    public int Score;
+
+    public void SetData(GameScript gameScript, Action callback)
+    {
+        _gameScript = gameScript;
+        _callback = callback;
+    }
+
+    public void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+
+    public void OnMouseDown()
+    {
+        gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0f);
+        if (gameObject.transform.localScale.x >= 1.5f)
+        {
+            DestroyEnemy();
+            _gameScript.Score += Score;
+            _gameScript.SetScore();
+            if (_callback != null)
+            {
+                _callback();
+            }
+        }
+    }
+}
