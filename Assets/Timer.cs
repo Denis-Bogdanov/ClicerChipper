a34c9a55ef519f5e9985afec39e881a16a8ccce7
Dennis Bogdanov <d.bogdanov@corp.mail.ru>
2019-07-16 18:59:15 +0300

    Добавил сцены

---
+++ Assets/Timer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text timer;
+    void Start()
+    {
+        InvokeRepeating("RunTimer", 1, 1);
+    }
+
+    void RunTimer()
+    {
+        timer.text = (int.Parse(timer.text) - 1).ToString();
+        if (int.Parse(timer.text) == 0)
+        {
+         CancelInvoke();
+         
+        }
+    }
+
+}
