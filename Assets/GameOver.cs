0883db9681f5cd2bdbea7e1e8a179f6f77e04c20
Dennis Bogdanov <d.bogdanov@corp.mail.ru>
2019-07-17 17:30:21 +0300

    Скрипт Сцены

---
+++ Assets/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+
+
+public class GameOver : MonoBehaviour {
+    public Text score;
+//    public GameScript GameScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       score.text = string.Format("Score {0}", GameScript.AllScore);
+    }
+
+
+
+}
